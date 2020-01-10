b0229ed50f7d695bf25ec505aaac5b82166752a4
Teo Jin Cheng <jinchengproject@gmail.com>
2020-01-10 16:43:37 +0800

    update add to use reduce function

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const add = function(numOne,numTwo){
-   return numOne + numTwo;
-}
+const add = function(...numbers) {
+  return numbers.reduce((acc, cur) => acc + cur, 0);
+};
 
 const math = {
-    add: add,
-}
-
+  add: add
+};
 
 module.exports = math;
